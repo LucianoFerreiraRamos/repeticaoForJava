76366dc0e095090b532e0028d3cadc9f760aec60
Luciano <97047885+LucianoFerreiraRamos@users.noreply.github.com>
2022-02-16 18:08:34 -0300

    RepeticaoFor

---
+++ src/repeticaofor/RepeticaoFor.java
@@ -0,0 +1,16 @@
+package repeticaofor;
+
+public class RepeticaoFor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int cc = 1; cc <= 100; cc+=10 ) {
+			System.out.println(cc);
+			
+		}
+				
+				
+
+	}
+
+}
